d0d56ad3e37fb66833d88af4dc7325897d46f7d2
Apeiron922003 <apeiron922003@gmail.com>
2024-11-28 03:42:18 +0700

    repush 60%

--- src/app/test/page.js
+++ src/app/test/page.js
@@ -1,9 +1,8 @@
-import Carousel from "@/components/Carousel";
 import ListItem from "@/components/ListItem";
 import React from "react";
 
 const TestPage = () => {
-  return <ListItem />;
+  return <>Test Page</>;
 };
 
 export default TestPage;
